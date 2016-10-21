233d5d122b45d0a3c3aa9b44ebdb406e38685335
Bianca Curutan <bmcurutan@gmail.com>
2016-10-20 22:26:06 -0500

    Basic Filters

---
+++ Yelp/FiltersViewController.swift
@@ -0,0 +1,105 @@
+//
+//  FiltersViewController.swift
+//  Yelp
+//
+//  Created by Bianca Curutan on 10/20/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol FiltersViewControllerDelegate {
+    @objc optional func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String:AnyObject])
+}
+
+class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SwitchCellDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    weak var delegate: FiltersViewControllerDelegate?
+    
+    var categories: [[String:String]]!
+    var switchStates = [Int:Bool]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        categories = yelpCategories()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK - IBAction
+    
+    @IBAction func onCancelButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onSearchButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+        var filters = [String:AnyObject]()
+        
+        var selectedCategories = [String]()
+        for (row, isSelected) in switchStates {
+            if isSelected {
+                selectedCategories.append(categories[row]["code"]!)
+            }
+        }
+        
+        if selectedCategories.count > 0 {
+            filters["categories"] = selectedCategories as AnyObject?
+        }
+        
+        delegate?.filtersViewController?(filtersViewController: self, didUpdateFilters: filters)
+    }
+    
+    // MARK: - Private Methods
+    
+    func yelpCategories() -> [[String:String]] {
+        return [
+            ["name": "Afghan", "code": "afghani"],
+            ["name": "French", "code": "french"]
+        ]
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath) as! SwitchCell
+        
+        cell.switchLabel.text = categories[indexPath.row]["name"]
+        cell.delegate = self
+        
+        cell.onSwitch.isOn = switchStates[indexPath.row] ?? false
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    // MARK: - SwitchCellDelegate
+    
+    func switchCell(switchCell: SwitchCell, didChangeValue value: Bool) {
+        let indexPath = tableView.indexPath(for: switchCell)!
+        
+        switchStates[indexPath.row] = value
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
